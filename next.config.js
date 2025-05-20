efdd286f2ce00672706d402f516cfd7dbe655a0e
prashantpoudel745 <prashantpoudel745@gmail.com>
2025-05-20 15:28:51 +0545

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "/portfolio_2",
+  assetPrefix: "/portfolio_2/",
   eslint: {
     ignoreDuringBuilds: true,
   },
